712e147a9fb23863535fb05550ac8c391f8a2b20
jolvs <jhz.joao_vinicius@gmail.com>
2025-06-12 02:07:32 -0300

    aula do dia 12

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 package src;
 
+import src.model.ContaBancaria;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Criando nosso Banco Digital");
         System.out.println();
+
+        ContaBancaria conta01 = new ContaBancaria("0001", "7542", 5, 100.00);
+
+        System.out.println(conta01.getSaldo());
     }
 }
--- src/model/ContaBancaria.java
+++ src/model/ContaBancaria.java
@@ -1,8 +1,51 @@
 package src.model;
 
 public class ContaBancaria {
+    
+    //#region ATRIBUTES
+    private String agencia;
+    private String conta;
+    private Integer digito;
+    private Double saldo;
+    //#region
 
-    public static void main(String[] args) {
-        
+    //#region CONSTRUTORES
+    public ContaBancaria(String agencia, String conta, Integer digito, Double saldoInicial) {
+        this.agencia = agencia;
+        this.conta = conta;
+        this.digito = digito;
+        this.saldo = saldoInicial;
     }
+    //#endregion
+    
+    //#region GETTERS
+    public String getAgencia() {
+        return agencia;
+    }
+    
+    public String getConta() {
+        return conta;
+    }
+    public Integer getDigito() {
+        return digito;
+    }
+    public Double getSaldo() {
+        return saldo;
+    }
+    //#endregion
+    
+    //#region SETTERS
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+    public void setDigito(Integer digito) {
+        this.digito = digito;
+    }
+    //#endregion
+
+
+
 } 
